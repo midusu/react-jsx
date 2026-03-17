8da154ffff221d2b9c75b833383558dcb6e493c1
midusu <midusummaakter@gmail.com>
2026-03-17 13:35:05 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 
 function App() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
@@ -15,6 +15,32 @@ function App() {
     <h1 style={{ textAlign: "center", marginTop: "50px" }}>
       Hello! Current Time: {time}
     </h1>
+  );
+}
+
+export default App;
+*/
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      {[1, 2, 3, 4, 5].map((item, index) => (
+        <h1 key={index}>
+          Hello! Current Time: {time}
+        </h1>
+      ))}
+    </div>
   );
 }
 
